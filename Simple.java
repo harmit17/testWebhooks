a0a8fb68543bb1d561fff16ccc260bf18e78fa65
Harrmit Chauhan <46776916+harmit17@users.noreply.github.com>
2023-02-28 16:23:34 -0500

    added one variable and printed on console for Simple.java

--- Simple.java
+++ Simple.java
@@ -1,7 +1,10 @@
 class Simple{  
     public static void main(String args[]){  
+        
+        int a = 5;
      System.out.println("Hello Java");  
         System.out.println("How are you??");
         System.out.println("I am doing well");
+        System.out.println(a);
     }  
 }  
