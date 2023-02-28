4be47374fefb3331c6cb52a3d944da998d862c64
Harrmit Chauhan <46776916+harmit17@users.noreply.github.com>
2023-02-28 16:26:28 -0500

    print a + b in Simple.java

--- Simple.java
+++ Simple.java
@@ -1,10 +1,10 @@
 class Simple{  
     public static void main(String args[]){  
         
-        int a = 5;
+        int a = 5, b = 5;
      System.out.println("Hello Java");  
         System.out.println("How are you??");
         System.out.println("I am doing well");
-        System.out.println(a);
+        System.out.println(a+b);
     }  
 }  
